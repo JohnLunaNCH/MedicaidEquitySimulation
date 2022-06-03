f61041bcb231276ad892a8d67d3de0aa90097495
JohnLunaNCH <john.luna@nationwidechildrens.org>
2022-06-03 15:20:40 -0400

    changes to module 2

--- scripts/module2nav.js
+++ scripts/module2nav.js
@@ -41,18 +41,52 @@ function Sect1(){
 	midSection.node().innerHTML = m2_typeNarration;
 	midSection.select("#narrationContainer").text("Please pay attention to how the dentist introduces the HPV vaccine and the communication techniques used to enhance health literacy.");
 	bottomSection.select(".bulletProgress").style("display", "none");
-	d3.select('#bottomContinueButton').attr("onClick", "Sect2()");
+	d3.select('#bottomContinueButton').attr("onClick", "Sect2a()");
 }
 
 
 //video
-function Sect2(){
+function Sect2a(){
 	titleSection.select('h1').text("");
 	ClearSection(midSection);
 	midSection.node().innerHTML = m2_typeVideo;
 	EnableButton(d3.select('#bottomContinueButton'), false);
 	//Scenario5 https://vimeo.com/manage/videos/677321017
-	midSection.select('iframe').attr('src', 'https://player.vimeo.com/video/677321017?h=882a3a78b2&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479');
+	midSection.select('iframe').attr('src', 'https://player.vimeo.com/video/716780801?h=78076ab601&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479');
+	
+	//wait til end of video to turn continue button on
+	var player = new Vimeo.Player(document.querySelector('iframe'));
+	player.on('ended',function(){EnableButton(d3.select('#bottomContinueButton'), true);});
+
+	bottomSection.select(".bulletProgress").style("display", "none");
+	d3.select('#bottomContinueButtonContainer').style("justify-content", "flex-end");
+	d3.select('#bottomContinueButton').attr("onClick", "Sect2b()");
+}
+
+function Sect2b(){
+	titleSection.select('h1').text("");
+	ClearSection(midSection);
+	midSection.node().innerHTML = m2_typeVideo;
+	EnableButton(d3.select('#bottomContinueButton'), false);
+	//Scenario5 https://vimeo.com/manage/videos/677321017
+	midSection.select('iframe').attr('src', 'https://player.vimeo.com/video/716778233?h=7cd69bafa4&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479');
+	
+	//wait til end of video to turn continue button on
+	var player = new Vimeo.Player(document.querySelector('iframe'));
+	player.on('ended',function(){EnableButton(d3.select('#bottomContinueButton'), true);});
+
+	bottomSection.select(".bulletProgress").style("display", "none");
+	d3.select('#bottomContinueButtonContainer').style("justify-content", "flex-end");
+	d3.select('#bottomContinueButton').attr("onClick", "Sect2c()");
+}
+
+function Sect2c(){
+	titleSection.select('h1').text("");
+	ClearSection(midSection);
+	midSection.node().innerHTML = m2_typeVideo;
+	EnableButton(d3.select('#bottomContinueButton'), false);
+	//Scenario5 https://vimeo.com/manage/videos/677321017
+	midSection.select('iframe').attr('src', 'https://player.vimeo.com/video/716781038?h=8ae793245c&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479');
 	
 	//wait til end of video to turn continue button on
 	var player = new Vimeo.Player(document.querySelector('iframe'));
@@ -136,18 +170,19 @@ function Sect7(){
 	titleSection.select('h1').text("");
 	ClearSection(midSection);
 	midSection.node().innerHTML = m2_typeNarration;
-	midSection.select("#narrationContainer").text("Watch the following scenario, can you spot the difference between this encounter and the previous encounter?");
+	midSection.select("#narrationContainer").text("The following videos are optional.");
 	bottomSection.select(".bulletProgress").style("display", "none");
-	d3.select('#bottomContinueButton').style("visibility","visible").attr("onClick", "Sect8()");
+	d3.select('#bottomContinueButton').style("visibility","visible").attr("onClick", "Sect8a()");
 }
 
 //Video
-function Sect8(){
+function Sect8a(){
 	titleSection.select('h1').text("");
 	ClearSection(midSection);
-	EnableButton(d3.select('#bottomContinueButton'), false);
+	EnableButton(d3.select('#bottomContinueButton'), true);
 	midSection.node().innerHTML = m2_typeVideo;
-	midSection.select('iframe').attr('src', 'https://player.vimeo.com/video/676021184?h=bb40bb226d&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479');
+	midSection.select('iframe').attr('src', 'https://player.vimeo.com/video/716778538?h=b084bfe60f&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479');
+
 	bottomSection.select(".bulletProgress").style("display", "none");
 	//wait til end of video to turn continue button on
 	var player = new Vimeo.Player(document.querySelector('iframe'));
@@ -157,11 +192,216 @@ function Sect8(){
 	d3.select('#bottomContinueButton').attr("onClick", "Sect9()");
 }
 
+function Sect8b(){
+	titleSection.select('h1').text("");
+	ClearSection(midSection);
+	EnableButton(d3.select('#bottomContinueButton'), true);
+	midSection.node().innerHTML = m2_typeVideo;
+	midSection.select('iframe').attr('src', 'https://player.vimeo.com/video/716782970?h=1fa186261e&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479');
+	
+	bottomSection.select(".bulletProgress").style("display", "none");
+	//wait til end of video to turn continue button on
+	var player = new Vimeo.Player(document.querySelector('iframe'));
+	player.on('ended',function(){EnableButton(d3.select('#bottomContinueButton'), true);});
+
+	d3.select('#bottomContinueButtonContainer').style("justify-content", "flex-end");
+	d3.select('#bottomContinueButton').attr("onClick", "Sect00NewEnding01()");
+}
+
 function EnableButton(button, turnOn){
 	if(turnOn)
 		button.style('visibility', 'visible');
 	else
 		button.style('visibility', 'hidden');
+}
+
+function Sect00NewEnding01(){
+	titleSection.select('h1').text("Why should dentists recommend HPV vaccine?");
+	ClearSection(midSection);
+	midSection.node().innerHTML = m2_typeMultipleChoice;
+	var buttonContainer = midSection.select('#buttonContainer');
+	buttonContainer.style("overflow-y", "auto");
+	buttonContainer.node().innerHTML = "";
+	var list = buttonContainer.append("ul");
+	list.append("li").text("The ADA adopted a policy in 2018 that urges dentists to support the use and administration of the HPV vaccine, recognizing it as a way to help prevent infection of the types of HPV associated with oropharyngeal cancer.");
+	list.append("li").text("An HPV workgroup led by ADA volunteers developed an evidence-based background report to help write the policy.");
+	list.append("li").text("By boosting HPV vaccination rates among your patients, you will be preventing cancer.");
+	list.append("li").text("Clinician recommendation is the number one reason parents decide to vaccinate.");
+
+	SetBulletProgress(10, 0);
+
+	d3.select('#bottomContinueButtonContainer').style("justify-content", "flex-start");
+	d3.select('#bottomContinueButton').text("Contine").style("visibility", "visible").attr('onClick', 'Sect00NewEnding02()');
+}
+
+function Sect00NewEnding02(){
+	titleSection.select('h1').text("How do I fit this into a busy practice?");
+	ClearSection(midSection);
+	midSection.node().innerHTML = m2_typeMultipleChoice;
+	var buttonContainer = midSection.select('#buttonContainer');
+	buttonContainer.style("overflow-y", "auto");
+	buttonContainer.node().innerHTML = "";
+	var list = buttonContainer.append("ul");
+	list.append("li").text("Starting with your front office, ensure each team member is aware of HPV vaccine’s importance and is educated on proper vaccination practices and recommendations, ready to answer parents’ questions, and/or regularly remind parents.  ");
+	list.append("li").text("Staff could regularly check immunization records, and let you know if parents have questions. ");
+	list.append("li").text("Use the resources of the local health department. ");
+	list.append("li").text("Be prepared to answer parents’ questions succinctly, accurately, and empathetically by using terms that they understand. A parent will often accept your explanations if presented with their children’s best interests in mind. ");
+	list.append("li").text("Clinician recommendation is the number one reason parents decide to vaccinate. ");
+
+	SetBulletProgress(10, 1);
+
+	d3.select('#bottomContinueButtonContainer').style("justify-content", "flex-start");
+	d3.select('#bottomContinueButton').text("Contine").style("visibility", "visible").attr('onClick', 'Sect00NewEnding03()');
+}
+
+function Sect00NewEnding03(){
+	titleSection.select('h1').text("Why does my child need HPV vaccination?");
+	ClearSection(midSection);
+	midSection.node().innerHTML = m2_typeMultipleChoice;
+	var buttonContainer = midSection.select('#buttonContainer');
+	buttonContainer.style("overflow-y", "auto");
+	buttonContainer.node().innerHTML = "";
+	var list = buttonContainer.append("ul");
+	list.append("li").text("HPV vaccination is important because it prevents infections that can cause cancer. ");
+	var subList = list.append("li").text("Other info if you want to include it:").append("ul");
+	subList.append("li").text("Human papillomavirus (HPV) vaccine protects against cancers caused by HPV infection. HPV is a common virus that infects teens and adults. About 1 million people, including teens, become infected with HPV each year. HPV infection can cause cervical, vaginal, and vulvar cancers in women and penile cancer in men. HPV can also cause anal cancer, cancer of the back of the throat (oropharynx), and genital warts in both men and women. ");
+	subList.append("li").text("Every year, about 36,000 men and women develop a cancer caused by HPV. HPV vaccination could prevent more than 90% of these cancers from ever developing. ");
+
+	SetBulletProgress(10, 2);
+
+	d3.select('#bottomContinueButtonContainer').style("justify-content", "flex-start");
+	d3.select('#bottomContinueButton').text("Contine").style("visibility", "visible").attr('onClick', 'Sect00NewEnding04()');
+}
+
+function Sect00NewEnding04(){
+	titleSection.select('h1').text("What diseases are caused by HPV?");
+	ClearSection(midSection);
+	midSection.node().innerHTML = m2_typeMultipleChoice;
+	var buttonContainer = midSection.select('#buttonContainer');
+	buttonContainer.style("overflow-y", "auto");
+	buttonContainer.node().innerHTML = "";
+	var list = buttonContainer.append("ul");
+	list.append("li").text("Some HPV infections can cause cancer—like cancer of the cervix, anus, penis, or in the back of the throat—but we can protect your child from getting these cancers in the future with HPV vaccination. ");
+
+	SetBulletProgress(10, 3);
+
+	d3.select('#bottomContinueButtonContainer').style("justify-content", "flex-start");
+	d3.select('#bottomContinueButton').text("Contine").style("visibility", "visible").attr('onClick', 'Sect00NewEnding05()');
+}
+
+function Sect00NewEnding05(){
+	titleSection.select('h1').text("How do you know HPV vaccination works? ");
+	ClearSection(midSection);
+	midSection.node().innerHTML = m2_typeMultipleChoice;
+	var buttonContainer = midSection.select('#buttonContainer');
+	buttonContainer.style("overflow-y", "auto");
+	buttonContainer.node().innerHTML = "";
+	var list = buttonContainer.append("ul");
+	list.append("li").text("Studies continue to prove HPV vaccination works extremely well, decreasing the number of infections and HPV precancers in people who have been vaccinated. ");
+
+	SetBulletProgress(10, 4);
+
+	d3.select('#bottomContinueButtonContainer').style("justify-content", "flex-start");
+	d3.select('#bottomContinueButton').text("Contine").style("visibility", "visible").attr('onClick', 'Sect00NewEnding06()');
+}
+
+function Sect00NewEnding06(){
+	titleSection.select('h1').text("Is my child really at risk for HPV infection?");
+	ClearSection(midSection);
+	midSection.node().innerHTML = m2_typeMultipleChoice;
+	var buttonContainer = midSection.select('#buttonContainer');
+	buttonContainer.style("overflow-y", "auto");
+	buttonContainer.node().innerHTML = "";
+	var list = buttonContainer.append("ul");
+	list.append("li").text("HPV is a very common infection in teens and adults, including women and men. Nearly everyone will get HPV at some point in their lives. Getting the vaccine will help protect your child from the cancers and diseases caused by HPV later in their lives.");
+
+	SetBulletProgress(10, 5);
+
+	d3.select('#bottomContinueButtonContainer').style("justify-content", "flex-start");
+	d3.select('#bottomContinueButton').text("Contine").style("visibility", "visible").attr('onClick', 'Sect00NewEnding07()');
+}
+
+function Sect00NewEnding07(){
+	titleSection.select('h1').text("Why do they need HPV vaccination at such a young age?");
+	ClearSection(midSection);
+	midSection.node().innerHTML = m2_typeMultipleChoice;
+	var buttonContainer = midSection.select('#buttonContainer');
+	buttonContainer.style("overflow-y", "auto");
+	buttonContainer.node().innerHTML = "";
+	var list = buttonContainer.append("ul");
+	list.append("li").text("Vaccines protect your child before they are exposed to an infection. That’s why we give HPV vaccination earlier rather than later, to protect them long before they are ever exposed.");
+	list.append("li").text("Also, if your child gets the shot before they turn 15, they will only need two doses. If you wait until your child is older, they will end up needing three shots.");
+
+	SetBulletProgress(10, 6);
+
+	d3.select('#bottomContinueButtonContainer').style("justify-content", "flex-start");
+	d3.select('#bottomContinueButton').text("Contine").style("visibility", "visible").attr('onClick', 'Sect00NewEnding08a()');
+}
+
+function Sect00NewEnding08a(){
+	titleSection.select('h1').text("");
+	ClearSection(midSection);
+	midSection.node().innerHTML = m2_typeMultipleChoice;
+	var buttonContainer = midSection.select('#buttonContainer');
+	buttonContainer.style("overflow-y", "auto");
+	buttonContainer.node().innerHTML = "";
+	buttonContainer.append("div").text("The following pages will provide guidance on how to answer parents' questions.");
+
+	SetBulletProgress(10, 7);
+
+	d3.select('#bottomContinueButtonContainer').style("justify-content", "flex-start");
+	d3.select('#bottomContinueButton').text("Contine").style("visibility", "visible").attr('onClick', 'Sect00NewEnding08()');
+}
+
+function Sect00NewEnding08(){
+	titleSection.select('h1').text("I’m worried my child will think that getting this vaccine makes it OK to have sex.");
+	ClearSection(midSection);
+	midSection.node().innerHTML = m2_typeMultipleChoice;
+	var buttonContainer = midSection.select('#buttonContainer');
+	buttonContainer.style("overflow-y", "auto");
+	buttonContainer.node().innerHTML = "";
+	var list = buttonContainer.append("ul");
+	list.append("li").text("Studies tell us that getting vaccinated doesn’t make kids more likely to start having sex. I made sure my child (or grandchild, etc.) got HPV vaccine.");
+
+	SetBulletProgress(10, 7);
+
+	d3.select('#bottomContinueButtonContainer').style("justify-content", "flex-start");
+	d3.select('#bottomContinueButton').text("Contine").style("visibility", "visible").attr('onClick', 'Sect00NewEnding09()');
+}
+
+function Sect00NewEnding09(){
+	titleSection.select('h1').text("Is HPV vaccination safe?");
+	ClearSection(midSection);
+	midSection.node().innerHTML = m2_typeMultipleChoice;
+	var buttonContainer = midSection.select('#buttonContainer');
+	buttonContainer.style("overflow-y", "auto");
+	buttonContainer.node().innerHTML = "";
+	var list = buttonContainer.append("ul");
+	list.append("li").text("Yes, HPV vaccination is very safe. Like any medicine, vaccines can cause side effects, including pain, swelling, or redness where the shot was given. That’s normal for HPV vaccine too and should go away in a day or two. Sometimes kids faint after they get shots and they could be injured if they fall from fainting.");
+	list.append("li").text("To prevent fainting and injuries from fainting, anyone receiving HPV vaccine should be seated or lying down during vaccination and for 15 minutes after getting the shot.");
+	list.append("li").text("Other info if you want to use it: With more than 135 million doses distributed in the United States, HPV vaccine has a reassuring safety record that is backed by over 15 years of monitoring and research.");
+
+	SetBulletProgress(10, 8);
+
+	d3.select('#bottomContinueButtonContainer').style("justify-content", "flex-start");
+	d3.select('#bottomContinueButton').text("Contine").style("visibility", "visible").attr('onClick', 'Sect00NewEnding10()');
+}
+
+function Sect00NewEnding10(){
+	titleSection.select('h1').text("How can I get help paying for these vaccines?");
+	ClearSection(midSection);
+	midSection.node().innerHTML = m2_typeMultipleChoice;
+	var buttonContainer = midSection.select('#buttonContainer');
+	buttonContainer.style("overflow-y", "auto");
+	buttonContainer.node().innerHTML = "";
+	var list = buttonContainer.append("ul");
+	list.append("li").text("The Vaccines for Children (VFC) program provides vaccines for children ages 18 years and younger who are uninsured, Medicaid-eligible, American Indian or Alaska Native. Learn more at www.cdc.gov/vaccines/programs/vfc/parents/qa-detailed.html");
+	list.append("li").text("You can also check with your local health department and with your health insurance.");
+
+	SetBulletProgress(10, 0);
+
+	d3.select('#bottomContinueButtonContainer').style("justify-content", "flex-start");
+	d3.select('#bottomContinueButton').text("Contine").style("visibility", "visible").attr('onClick', 'End()');
 }
 
 function Sect9(){
